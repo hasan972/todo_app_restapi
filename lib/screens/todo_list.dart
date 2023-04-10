b708615138c72a44e287930ac8b2d0169b19f6b8
Md: Rakibul Hasan <49594744+hasan972@users.noreply.github.com>
2023-04-11 00:09:52 +0600

    Initial commit

---
+++ lib/screens/todo_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_restapi/screens/add_page.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed:navigateToAddPage,
+        label: const Text('Add'),
+      ),
+    );
+  }
+
+  //.........This is the methode for go to add_page..........
+  void navigateToAddPage() {
+    final route = MaterialPageRoute(
+      builder: (context) => const AddTodoPage(),
+    );
+    Navigator.push(context, route);
+  }
+}
